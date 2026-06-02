660bf096275831f3449203e07fd7d95aa6c0fa4b
brucewayne <151868514+aliframework@users.noreply.github.com>
2026-06-02 11:22:16 +0700

    Update test to assert single product found

--- tests/CatalogTest.php
+++ tests/CatalogTest.php
@@ -30,7 +30,7 @@ class CatalogTest extends TestCase
     {
         $result = $this->katalog->searchProduct("Kemeja");
 
-        $this->assertCount(99, $result);
+        $this->assertCount(1, $result);
     }
 
     protected function tearDown(): void
